b53810c2b96fe4adcaa5b4542770c9669f8193c4
Danya <danatahtarov09@gmail.com>
2024-11-28 14:50:26 +0200

    add new desing for web

--- src/components/NewsBanner.jsx
+++ src/components/NewsBanner.jsx
@@ -1,10 +1,9 @@
 import { formatTimeAgo } from '../helpers/formatTimeAgo';
-import { withSkeleton } from '../helpers/hocs/withSkeleton';
 import { Image } from './Image';
 
-function NewsBanner({ item }) {
+export function NewsBanner({ item }) {
   return (
-    <div className="flex gap-3 flex-col">
+    <li className="flex gap-3 flex-col">
       <Image image={item?.image} />
       <div className="flex flex-col flex-1 gap-2">
         <h3 className="font-semibold text-lg leading-6">{item?.title}</h3>
@@ -12,8 +11,6 @@ function NewsBanner({ item }) {
           item?.published
         )} by ${item?.author}`}</p>
       </div>
-    </div>
+    </li>
   );
 }
-
-export const NewsBannerWhithSkeleton = withSkeleton(NewsBanner, 'lg', 1);
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
         inter: 'Inter, sans-serif',
       },
     },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    '2xl': '1536px',
+    },
   },
   plugins: [require('tailwind-scrollbar')],
 };
